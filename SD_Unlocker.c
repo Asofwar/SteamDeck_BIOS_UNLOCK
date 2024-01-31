70442b6db74e766bd93e7bfb8a634870dd00d129
Андрей Зубченко <Asofwar@users.noreply.github.com>
2024-01-31 17:50:07 +0300

    Update SD_Unlocker.c
    фикс логов

--- SD_Unlocker.c
+++ SD_Unlocker.c
@@ -3,26 +3,33 @@
 #include <sys/io.h>
 
 int main() {
+    // Предоставляем разрешение на доступ к портам ввода/вывода
     if (ioperm(0x75, 2, 1) != 0) {
         perror("Error granting I/O permission");
         exit(EXIT_FAILURE);
     }
 
-    // Считываем значения из портов
-    unsigned char value1 = inb(0x75);
-    unsigned char value2 = inb(0x76);
+    // Считываем начальные значения из портов
+    unsigned char initial_value1 = inb(0x75);
+    unsigned char initial_value2 = inb(0x76);
 
-    printf("Initial values: 0x%02X, 0x%02X\n", value1, value2);
+    printf("Initial values: 0x%02X, 0x%02X\n", initial_value1, initial_value2);
 
     // Изменяем значения на портах
     outb(0xF7, 0x75);
     outb(0x77, 0x76);
 
     // Повторно считываем измененные значения
-    value1 = inb(0x75);
-    value2 = inb(0x76);
+    unsigned char updated_value1 = inb(0x75);
+    unsigned char updated_value2 = inb(0x76);
 
-    printf("Updated values: 0x%02X, 0x%02X\n", value1, value2);
+    printf("Updated values: 0x%02X, 0x%02X\n", updated_value1, updated_value2);
+
+    // Завершаем работу, освобождаем разрешения
+    if (ioperm(0x75, 2, 0) != 0) {
+        perror("Error revoking I/O permission");
+        exit(EXIT_FAILURE);
+    }
 
     printf("I/O operations completed successfully.\n");
 
