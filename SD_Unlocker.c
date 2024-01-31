1a65f4bcf2a24fc9a1c83ef23b94ac4bb81620f0
Андрей Зубченко <Asofwar@users.noreply.github.com>
2024-01-31 17:21:32 +0300

    Update SD_Unlocker.c
    лог в терминал

--- SD_Unlocker.c
+++ SD_Unlocker.c
@@ -1,6 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/io.h>
-void main(){
-    ioperm(0x75,2,1);
-    outb(0xF7,0x75);
-    outb(0x77,0x76);
+
+int main() {
+    if (ioperm(0x75, 2, 1) != 0) {
+        perror("Error granting I/O permission");
+        exit(EXIT_FAILURE);
+    }
+
+    // Считываем значения из портов
+    unsigned char value1 = inb(0x75);
+    unsigned char value2 = inb(0x76);
+
+    printf("Initial values: 0x%02X, 0x%02X\n", value1, value2);
+
+    // Изменяем значения на портах
+    outb(0xF7, 0x75);
+    outb(0x77, 0x76);
+
+    // Повторно считываем измененные значения
+    value1 = inb(0x75);
+    value2 = inb(0x76);
+
+    printf("Updated values: 0x%02X, 0x%02X\n", value1, value2);
+
+    printf("I/O operations completed successfully.\n");
+
+    return 0;
 }
